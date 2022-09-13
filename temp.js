c2345951ec9057aa04d8a80d2a37c965dcb13960
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-09-13 19:44:25 +0600

    first commit

---
+++ temp.js
@@ -0,0 +1,11 @@
+// Problem Task :
+// * Accept a number from user. (Temperature in Celcius) 
+// * Convert it to Fahrenheit (Formula: F = 9/5*C + 32)
+// * Print the Result as alert! ("Fahrenheit: 94 Degree")
+// * Print "Task Complete" in Console!
+
+var temp = prompt("Please enter a temperature: ");
+var result = 9/5*temp + 32;
+alert("Fahrenheit: " + result + "Degree");
+
+console.log("Task Complete!");
